885a6e2b0c7c2d93442acf64e3bd9e2012b6b926
isando <isaac@sandofarms.com>
2016-10-31 17:37:05 -0500

    only return .md files when searching docs dirs

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import glob
 import markdown
 import os
 
@@ -17,7 +18,9 @@ def _get_doc_dirs():
     return os.listdir(DOCS_ROOT)
 
 def _get_docs_by_dir(dir_name):
-    return os.listdir(os.path.join(DOCS_ROOT, dir_name))
+    # return only .md files
+    return [os.path.basename(f)
+            for f in glob.glob(os.path.join(DOCS_ROOT, dir_name, '*.md'))]
 
 def _get_html_from_md(doc_type, md_file):
     doc_file = os.path.join(DOCS_ROOT, doc_type, md_file)
